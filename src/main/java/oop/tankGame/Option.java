4c06f51274adb7612db616be7371f5a8929e9af4
laivx <laivx_55@vnu.edu.vn>
2014-08-12 21:42:26 +0700

    Update Option.java

--- src/main/java/oop/tankGame/Option.java
+++ src/main/java/oop/tankGame/Option.java
@@ -2,7 +2,7 @@ package main.java.oop.tankGame;
 
 public class Option {
     //public static String homeDir = null;
-    public static String backgroundDir = "C:\\Users\\VuXuan\\Desktop\\tankGame-master2\\tankGame-master\\etc\\Backgrounds";
-    public static String imageDir = "C:\\Users\\VuXuan\\Desktop\\tankGame-master2\\tankGame-master\\etc\\Images";
+    public static String backgroundDir = "E:\\workspace\\tankGame-master2\\tankGame-master\\etc\\Backgrounds";
+    public static String imageDir = "E:\\workspace\\tankGame-master2\\tankGame-master\\etc\\Images";
     public static String FILE_SEPARATOR = System.getProperty("file.separator");
 }
